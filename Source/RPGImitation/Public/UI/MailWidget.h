a7cc74d1d8136e1aa20f37ecd6a99b9e80776a64
Seoksum <ssm9804@naver.com>
2024-09-20 17:57:47 +0900

    Edit Mail

---
+++ Source/RPGImitation/Public/UI/MailWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MailWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPGIMITATION_API UMailWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
